3bcca739281ad772057d56642d3cf94f34de6ffd
wwangnju <wwangnju@163.com>
2024-09-25 12:12:18 +0800

    edit_link="main"

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(;
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://wwangnju.github.io/PhysicsEducation.jl",
-        edit_link="master",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
